ad58eda668c03c8d208e0012ee6cb572f1f474c3
Pratibha <pratibhasutar6451@gmail.com>
2023-04-21 16:24:54 +0530

    commit

--- models/Attendences.js
+++ models/Attendences.js
@@ -2,14 +2,11 @@ var Database = require("./Database");
 class Attendences{
     constructor(){
         this.id = 0;
-        this.category_id = 0;
-        this.employee_details_id = 0;
-        this.name = "";
-        this.mobile_no ="";
         this.date = "";
+        this.category_id = 0;
+        this.name = "";
         this.present = "";
         this.absent = "";
-        this.attendence = "";
 
         this.db = new Database();
         this.query ="";
@@ -18,24 +15,18 @@ class Attendences{
 //save record
 save = ()=>{
     this.query = 
-    "INSERT INTO attendences(date, category_id, employee_details_id, name,mobile_no, present, absent, attendence)";
+    "INSERT INTO attendences(date, category_id, name, present, absent)";
     this.query +=
     "VALUES('" + 
     this.date +
     "', '" + 
     this.category_id +
     "', '" + 
-    this.employee_details_id +
-    "', '" + 
     this.name +
-    "', '" + 
-    this.mobile_no +
     "', '" + 
     this.present +
     "', '" + 
     this.absent +
-    "', '" + 
-    this.attendence +
     "')";
     return new Promise((resolve, reject)=>{
            this.db.query(this.query,(err, result)=>{
@@ -49,7 +40,7 @@ save = ()=>{
 }
 //update record
 update = ()=>{
-    this.query = "UPDATE attendences SET date ='" + this.date +  "', attendence ='" + this.attendence +  "' WHERE id =" + this.id;
+    this.query = "UPDATE attendences SET date ='" + this.date +  "', present ='" + this.present + "',absent ='" + this.absent  +  "' WHERE id =" + this.id;
 
     return new Promise((resolve, reject)=>{
 
@@ -79,36 +70,6 @@ list = ()=>{
 
     });
 }
-
-// lists = ()=>{
-//     // this.query = "SELECT * FROM attendences ORDER BY id";
-//     this.query = "SELECT * FROM attendences ORDER BY category_id";
-
-//     return new Promise((resolve, reject)=>{
-
-//         this.db.query(this.query,(err, result)=>{
-//             if(err)
-//             reject(err);
-//             resolve(result);
-//         });
-
-//     });
-// }
-
-// get = ()=>{
-    
-//     this.query = "SELECT * FROM attendences WHERE employee_details_id = " + this.employee_details_id + " ORDER BY id";
-
-//     return new Promise((resolve, reject)=>{
-
-//         this.db.query(this.query,(err, result)=>{
-//             if(err)
-//             reject(err);
-//             resolve(result);
-//         });
-
-//     });
-// }
 //get single record
 get = ()=>{
     this.query = "SELECT * FROM attendences WHERE id =" + this.id;
--- routes/attendences.js
+++ routes/attendences.js
@@ -6,16 +6,11 @@ var router = express.Router();
 router.put("/save",  async(req, res) => {
     let body = req.body;
     let attendences = new Attendences();
-    attendences.category_id = body.category_id;
-    attendences.employee_details_id = body.employee_details_id;
-    attendences.name = body.name;
-    attendences.mobile_no = body.mobile_no;
     attendences.date = body.date;
+    attendences.category_id = body.category_id;
+    attendences.name = body.name;
     attendences.present = body.present;
     attendences.absent = body.absent;
-
-
-    attendences.attendence = body.attendence;
      attendences.save().then((result) => {
      res.end(JSON.stringify({ status: "success", data:result  }));
     }, (err) => {
@@ -32,12 +27,11 @@ router.post("/update", async (req, res)=>{
     let body = req.body;
        let attendences = await new Attendences();
     attendences.id = body.id;
-       attendences.date = body.date;
-       attendences.name = body.name;
-       attendences.mobile_no = body.mobile_no;
-       attendences.present = body.present;
+    attendences.date = body.date;
+    attendences.category_id = body.category_id;
+    attendences.name = body.name;
+    attendences.present = body.present;
     attendences.absent = body.absent;
-       attendences.attendence = body.attendence;
        attendences.update().then((result)=>{
        res.end(JSON.stringify({status:"success", data:result}));
        }, (err)=>{
@@ -60,52 +54,6 @@ router.get("/list", async (req, res) => {
     });
     
 });
-// router.get("/lists/:category_id", (req, res) => {
-//     let attendences = new Attendences();
-//     attendences.category_id = req.params.category_id;
-//     attendences.lists().then((result) => {
-//         res.end(JSON.stringify({ status: "success", data: result }));
-//     }, (err) => {
-
-//         res.end(JSON.stringify({ status: "failed", data: err }));
-
-//     });
-    
-// });
-// //get single record
-// router.get("/:category_id/:id", (req, res) => {
-//     let attendences = new Attendences();
-//     attendences.id = req.params.id;
-//     attendences.get().then((result) => {
-//         res.end(JSON.stringify({ status: "success", data: result }));
-//     }, (err) => {
-
-//         res.end(JSON.stringify({ status: "failed", data: err }));
-
-//     });
-    
-// });
-// router.get("/:category_id/:id",(req,res)=>{
-//     employee_details.findById(req.params.id).then((result)=>{   
-//         res.end(JSON.stringify({status:"success", data:result}));
-//     },(err)=>{
-//         res.end(JSON.stringify({status:"failed", data:err}));
-//     })
-// });
-// router.get("/:employee_details_id/:id", (req, res) => {
-
-//     let attendences = new Attendences();
-//     attendences.id = req.params.id;
-//     attendences.list().then((result) => {
-//         res.end(JSON.stringify({ status: "success", data: result }));
-//     }, (err) => {
-
-//         res.end(JSON.stringify({ status: "failed", data: err }));
-
-//     });
-    
-// });
-
 //delete record
 router.delete("/delete/:id", (req, res)=>{
     let attendences = new Attendences();
